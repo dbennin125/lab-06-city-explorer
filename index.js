5aef762fcada76f7d542f59d071a2a357da992fb
Daniel Bennington <dbennington125@gmail.com>
2020-05-06 13:43:02 -0700

    try again

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) =>{
-    res.json({ hello: 'world' });
+app.get('/location/:id', (req, res) =>{
+    // console.log('anything');
+    res.json({ 
+        'formatted_query': 'Seattle, WA, USA',
+        'latitude': '47.606210',
+        'longitude': '-122.332071'
+    });
 });
+
 
 app.listen(PORT, () => console.log(`running on port${PORT}`));
